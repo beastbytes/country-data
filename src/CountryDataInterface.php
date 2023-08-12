b17a0cb005c05084fe6e13f51339cc6973d1dd24
Chris Yates <chris@clyates.plus.com>
2023-08-12 15:17:44 +0100

    Add doc blocks

--- src/CountryDataInterface.php
+++ src/CountryDataInterface.php
@@ -11,24 +11,51 @@ namespace BeastBytes\Country;
 interface CountryDataInterface
 {
     /**
-     * @return array ISO 3116 alpha-2 codes of supported countries
+     * @return array List (e.g. ISO 3116 alpha-2 codes) of supported countries
      */
     public function getCountries(): array;
-
     /**
      * @return bool Whether the country is in the list of supported countries
      */
     public function hasCountry(string $country): bool;
-    public function formatAddress(array $address, string $country): string;
-    public function formatName(array $name, string $country): string;
+    /**
+     * @return string Address format for the country
+     */
     public function getAddressFormat(string $country): string;
+    /**
+     * @return string ISO 3116 alpha-3 code for the country
+     */
     public function getAlpha3(string $country): string;
+    /**
+     * @return array All data for the country
+     */
     public function getCountry(string $country): array;
+    /**
+     * @return string International dialling code for the country
+     */
     public function getIdc(string $country): string;
+    /**
+     * @return string Flag for the country
+     */
     public function getFlag(string $country): string;
+    /**
+     * @return string Name of the country
+     */
     public function getName(string $country): string;
+    /**
+     * @return string Name format for the country
+     */
     public function getNameFormat(string $country): string;
+    /**
+     * @return string ISO 3116 numeric code for the country
+     */
     public function getNumericCode(string $country): string;
+    /**
+     * @return string Top level domain for the country
+     */
     public function getTld(string $country): string;
+    /**
+     * @return string Timezone(s) for the country
+     */
     public function getTimezone(string $country): string;
 }
